ef7a4cc0491447e71332fac9c35aa7935ec9ef48
chhong <pnci1029@gmail.com>
2024-07-22 21:20:12 +0900

    add library (react, typescript, react-router-dom,

--- koreer-fe/src/App.tsx
+++ koreer-fe/src/App.tsx
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>123123
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +17,9 @@ function App() {
           hello world
         </a>
       </header>
-    </div>
+      </div>
+    // <div className="App">
+    // </div>
   );
 }
 

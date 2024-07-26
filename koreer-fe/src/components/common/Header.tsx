5120530f8dbe681c45eeb0a972bc64d9c87b5e8b
chhong <pnci1029@gmail.com>
2024-07-26 14:05:18 +0900

    add new component Header and make structures about style components

---
+++ koreer-fe/src/components/common/Header.tsx
@@ -0,0 +1,11 @@
+import style from "../../assets/scss/common/header.module.scss";
+
+export function Header() {
+    return(
+        <>
+            <header className={style.header}>
+                this is header
+            </header>
+        </>
+    )
+}

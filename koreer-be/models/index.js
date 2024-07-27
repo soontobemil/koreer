ada84f1a8b81542d7c86240e21807adb7c887b92
yeahsdev <iyeahs71@gmail>
2024-07-27 16:33:32 +0900

    Separated database configuration files and Create User Table model file

---
+++ koreer-be/models/index.js
@@ -0,0 +1,26 @@
+// models/index.js
+const { Sequelize } = require('sequelize');
+const config = require('../config/config');
+const fs = require('fs');
+const path = require('path');
+
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.dialect
+});
+
+const db = {};
+
+fs.readdirSync(__dirname)
+  .filter(file => file.indexOf('.') !== 0 && file !== 'index.js')
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;

84f2f8bfcf091aa69342b569ee0f864ce8ed62af
chhong <pnci1029@gmail.com>
2024-07-29 21:18:30 +0900

    add header style for common components

--- koreer-fe/src/components/common/Header.tsx
+++ koreer-fe/src/components/common/Header.tsx
@@ -1,11 +1,18 @@
 import style from "../../assets/scss/common/header.module.scss";
 
 export function Header() {
-    return(
+
+    return (
         <>
             <header className={style.header}>
-                this is header
+                <div className={style.headerButtonWrapper}>
+                    <button>About us</button>
+                    <button>Community</button>
+                    <button>Job Information</button>
+                    <button>Contact</button>
+                    <button>Login</button>
+                </div>
             </header>
         </>
-    )
+    );
 }

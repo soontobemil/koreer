4b8c36d78d430ef6447b22cfdccd06bd5e1376a0
chhong <pnci1029@gmail.com>
2024-07-31 19:40:43 +0900

    fix main title style in main.module.scss & add main title named 'koreer' in Main component and css

--- koreer-fe/src/components/common/Main.tsx
+++ koreer-fe/src/components/common/Main.tsx
@@ -1,11 +1,33 @@
 import {Header} from "./Header";
 import style from "../../assets/scss/common/main.module.scss"
+import {useState} from "react";
 
 export default function Main(){
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleHamburgerClick = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return(
         <>
         <Header/>
         <div className={style.mainContainer}>
+
+            {/* 메인 타이틀 부분 */}
+            <div className={style.mainHeaderWrapper}>
+                <button className={style.mainTitleWrapper}>
+                    <span className={style.mainTitleText}>Koreer</span>
+                </button>
+                <div className={style.hamburgerMenu} onClick={handleHamburgerClick}>
+                    <div className={style.bar}></div>
+                    <div className={style.bar}></div>
+                    <div className={style.bar}></div>
+                </div>
+            </div>
+
+            {/* 북미 회사 조회 컴포넌트 분리 예정 */}
             <div className={style.questionContainer}>
                 <div className={style.questionText}>북미 취업을 원해?</div>
                 <div className={`${style.arrowImage} ${style.arrowImageRight}`}></div>
@@ -15,6 +37,9 @@ export default function Main(){
                     <button className={style.questionButton}>USA</button>
                 </div>
             </div>
+
+
+
         </div>
 </>
 )

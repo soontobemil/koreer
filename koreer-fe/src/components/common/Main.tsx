a58a63a58955c1d5d744c1d8801fd5d6f3e3eddf
chhong <pnci1029@gmail.com>
2024-08-01 18:58:34 +0900

    fix some css in mainCommunityWrapper which contains hover and  transitions

--- koreer-fe/src/components/common/Main.tsx
+++ koreer-fe/src/components/common/Main.tsx
@@ -47,31 +47,6 @@ export default function Main(){
                 </div>
 
                 <ListItems />
-                {/*<div className={style.listBoxWrapper}>*/}
-                {/*    <div className={style.listItems}>*/}
-                {/*        <div className={style.listTitleWrapper}>*/}
-                {/*            <span className={style.listCategory}>꿀팁</span>*/}
-                {/*            <span className={style.listTitle}>캐나다 항공 공짜로 구하는 법</span>*/}
-                {/*        </div>*/}
-                {/*        <div className={style.listContentWrapper}>*/}
-
-                {/*            <span className={style.listDescription}>1. 스카이스캐너에 접속한다. 2. 환경설정에서 하단의...</span>*/}
-                {/*        </div>*/}
-
-                {/*        <div className={style.listIconWrapper}>*/}
-                {/*            <FavoriteIcon className="add"/>*/}
-                {/*            <div className="read">*/}
-                {/*                <VisibilityIcon className="read_icon" />*/}
-                {/*                <div className="read_count">21</div>*/}
-                {/*            </div>*/}
-                {/*            <div className="comment">*/}
-                {/*                <CommentIcon className="comment_icon" />*/}
-                {/*                <div className="comment_count">3</div>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-
-                {/*    </div>*/}
-                {/*</div>*/}
             </div>
 
 

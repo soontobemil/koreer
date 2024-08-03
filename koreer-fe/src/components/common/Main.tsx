f1b3502a2dfb790ab735092ac8a8980515e571a8
chhong <pnci1029@gmail.com>
2024-08-03 13:16:47 +0900

    add testimonials components named by testimonialField and add some dummy data for test

--- koreer-fe/src/components/common/Main.tsx
+++ koreer-fe/src/components/common/Main.tsx
@@ -2,9 +2,11 @@ import {Header} from "./Header";
 import style from "../../assets/scss/common/main.module.scss"
 import {useState} from "react";
 import {ListItems} from "../ListItems";
-import {HiringProcessField} from "./hiringprocess/HiringProcessField";
+import {HiringProcessField} from "./main/HiringProcessField";
+import {CompanyInformationField} from "./main/CompanyInformationField";
+import {TestimonialField} from "./main/TestimonialField";
 
-export default function Main(){
+export default function Main() {
 
     const [menuOpen, setMenuOpen] = useState(false);
 
@@ -12,50 +14,43 @@ export default function Main(){
         setMenuOpen(!menuOpen);
     };
 
-    return(
+    return (
         <>
-        <Header/>
-        <div className={style.mainContainer}>
+            <Header/>
+            <div className={style.mainContainer}>
 
-            {/* 메인 타이틀 부분 */}
-            <div className={style.mainHeaderWrapper}>
-                <button className={style.mainTitleWrapper}>
-                    <span className={style.mainTitleText}>Koreer</span>
-                </button>
-                <div className={style.hamburgerMenu} onClick={handleHamburgerClick}>
-                    <div className={style.bar}></div>
-                    <div className={style.bar}></div>
-                    <div className={style.bar}></div>
-                </div>
-            </div>
-
-            {/* 북미 회사 조회 컴포넌트 분리 예정 */}
-            <div className={style.questionContainer}>
-                <div className={style.questionText}>북미 취업을 원해?</div>
-                <div className={`${style.arrowImage} ${style.arrowImageRight}`}></div>
-                <div className={`${style.arrowImage} ${style.arrowImageLeft}`}></div>
-                <div className={style.buttonsContainer}>
-                    <button className={style.questionButton}>Canada</button>
-                    <button className={style.questionButton}>USA</button>
-                </div>
-            </div>
-
-        {/*  커뮤니티 컴포넌트 분리예정  */}
-            <div className={style.mainCommunityWrapper}>
-                <div className={style.communityTitleWrapper}>
-                    <div className={style.titleTop}>TODAY HOT ARTICLE</div>
-                    <div className={style.titleMain}>커뮤니티 인기 게시글 TOP 3</div>
+                {/* 메인 타이틀 부분 */}
+                <div className={style.mainHeaderWrapper}>
+                    <button className={style.mainTitleWrapper}>
+                        <span className={style.mainTitleText}>Koreer</span>
+                    </button>
+                    <div className={style.hamburgerMenu} onClick={handleHamburgerClick}>
+                        <div className={style.bar}></div>
+                        <div className={style.bar}></div>
+                        <div className={style.bar}></div>
+                    </div>
                 </div>
 
-                <ListItems />
+                {/* 북미 회사 조회 컴포넌트 분리 예정 */}
+                <CompanyInformationField/>
+
+                {/*  커뮤니티 컴포넌트 분리예정  */}
+                <div className={style.mainCommunityWrapper}>
+                    <div className={style.communityTitleWrapper}>
+                        <div className={style.titleTop}>TODAY HOT ARTICLE</div>
+                        <div className={style.titleMain}>커뮤니티 인기 게시글 TOP 3</div>
+                    </div>
+
+                    <ListItems/>
+                </div>
+
+
+                {/*  북미 취업 과정 컴포넌트  */}
+                <HiringProcessField/>
+
+                {/*  평가 및 후기  */}
+                <TestimonialField />
             </div>
-
-
-        {/*  북미 취업 과정 컴포넌트  */}
-            <HiringProcessField />
-
-
-        </div>
         </>
     )
 }

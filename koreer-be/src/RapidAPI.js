90407966b6d0794039499bd774522f76a2d55301
yeahsdev <iyeahs71@gmail>
2024-08-10 16:32:50 +0900

    Create API endpoints for RapidAPI and Complete to make Cron Job Process

--- koreer-be/src/RapidAPI.js
+++ koreer-be/src/RapidAPI.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
 
-const options = {
-  method: 'GET',
+// 24/08/01 exceeded the MONTHLY quota..
+const options1 = {
+  method: 'GET',  
   url: 'https://linkedin-data-api.p.rapidapi.com/search-jobs',
   params: { 
-    query: 'keywords=python&locationId=92000000&datePosted=anyTime&sort=mostRelevan' 
+    query: 'keywords=Node.js&locationId=103366113&datePosted=pastMonth&sort=mostRelevan' 
     },
   headers: {
     'X-RapidAPI-Host': 'linkedin-data-api.p.rapidapi.com',
@@ -12,9 +13,40 @@ const options = {
   }
 };
 
+const options2 = {
+    method: 'GET',  
+    url: 'https://linkedin-data-scraper.p.rapidapi.com/search_jobs',
+    params:{
+      query: 'Node.js',
+      page:1,
+      searchLocationId:'103366113',
+      experience:'3,4',
+      postedAgo:86400,
+      sortBy:'R'
+    },
+    headers: {
+      'X-RapidAPI-Host': 'linkedin-data-scraper.p.rapidapi.com',
+      'X-RapidAPI-Key': '858b17a39amsh15f502e313e8cffp10c854jsnfeac807c2e37'
+    }
+  };
+const options3 = {
+    method: 'POST',  
+    url: 'https://linkedin-data-scraper.p.rapidapi.com/search_jobs',
+    headers: {
+        'X-RapidAPI-Host': 'linkedin-data-scraper.p.rapidapi.com',
+        'X-RapidAPI-Key': '858b17a39amsh15f502e313e8cffp10c854jsnfeac807c2e37',
+        'Content-Type': 'application/json' 
+    },
+    data :{
+        "keywords": "Mern Stack Developer",
+        "location": "California, United States",
+        "count": 10
+    }
+};
+
 async function fetchJobListings() {
   try {
-    const response = await axios.request(options);
+    const response = await axios.request(options2);
     console.log('TOTAL COUNTS :', response.data.length);
     console.log('Full Response Data:', JSON.stringify(response.data, null, 4));
     if (Array.isArray(response.data)) {
---
+++ koreer-be/src/common/constants.js
@@ -0,0 +1,13 @@
+const ERROR_MESSAGES = {
+    USER_NOT_FOUND: 'User not found',
+};
+
+const COUNTRY_INFOS = {
+    ['Canada']:{
+        LOCATION_ID: '103366113',
+        COUNTRY_CODE: 'ca',
+        CITY_1:'Vancouver',
+    }
+};
+
+module.exports = { ERROR_MESSAGES,COUNTRY_INFOS };

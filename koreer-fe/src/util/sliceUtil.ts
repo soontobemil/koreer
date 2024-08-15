cc13953334904e7ecb5eebc0008ec8f8240dfc3e
chhong <pnci1029@gmail.com>
2024-08-15 14:46:45 +0900

    set up typescript files for connecting api

---
+++ koreer-fe/src/util/sliceUtil.ts
@@ -0,0 +1,27 @@
+import { ActionReducerMapBuilder, AsyncThunk } from "@reduxjs/toolkit";
+import { SliceState, Status } from "../types/common";
+import { AxiosResponse } from "axios";
+
+export const addCases = <T extends SliceState>(
+  builder: ActionReducerMapBuilder<T>,
+  asyncThunk: AsyncThunk<any, any, {}>
+) => {
+  builder
+    .addCase(asyncThunk.pending, (state) => {
+      state.status = Status.LOADING;
+    })
+    .addCase(asyncThunk.rejected, (state, action) => {
+      state.status = Status.FAILED;
+    })
+    .addCase(asyncThunk.fulfilled, (state) => {
+      state.status = Status.IDLE;
+    });
+};
+
+export const executePromise = async <T>(f: () => Promise<AxiosResponse<T>>) => {
+  try {
+    return await f();
+  } catch (error: any) {
+    throw new Error(JSON.stringify(error?.response?.data));
+  }
+};

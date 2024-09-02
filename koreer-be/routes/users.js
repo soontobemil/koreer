1c0cc711c81109e36e5bcdf22cce7535133db666
yeahsdev <iyeahs71@gmail>
2024-09-03 00:38:53 +0900

    Separate Duplicate User Check API

--- koreer-be/routes/users.js
+++ koreer-be/routes/users.js
@@ -3,13 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 /* GET users listing. */
-/*
-router.get('/', function(req, res, next) {
-    res.send('GET Succcess respond with a resource');
-  });  
-*/
 router.get('/:email', userController.getUserByEmail); // get url such as 'localhost:3000/users/mail@example.com'
 router.post('/', userController.createUser);
+router.get('/dupl-check/:email', userController.userDuplCheck);
 
 
 module.exports = router;

82d7c81924a7a3ddd91df794fb6d7f41f1bbb85a
chhong <pnci1029@gmail.com>
2024-09-03 20:55:10 +0900

    create functions for checking user's geography informations

--- koreer-fe/src/components/signup/SignUpNationField.tsx
+++ koreer-fe/src/components/signup/SignUpNationField.tsx
@@ -1,5 +1,5 @@
 import style from "../../assets/scss/sub/signup.module.scss";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 interface Args{
     nation: string;
@@ -23,6 +23,66 @@ export function SignUpNationField({nation, setNation}:Args) {
     const selectNation = (value:string) =>{
         setNation(value)
     }
+
+    // 국가 체크 부분
+    const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setLocation({
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                    });
+                },
+                (error) => {
+                    console.log(error)
+                }
+            );
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    }, []);
+
+    useEffect(() => {
+        const checkNation = () => {
+            const { latitude, longitude } = location;
+
+            const locations = [
+                {
+                    name: 'KOREA',
+                    latRange: [37, 38],
+                    lonRange: [126, 127],
+                },
+                {
+                    name: 'USA',
+                    latRange: [24, 50],
+                    lonRange: [-126, -66],
+                },
+                {
+                    name: 'CANADA',
+                    latRange: [41, 84],
+                    lonRange: [-142, -52],
+                },
+            ];
+
+            for (const loc of locations) {
+                if (
+                    latitude >= loc.latRange[0] && latitude <= loc.latRange[1] &&
+                    longitude >= loc.lonRange[0] && longitude <= loc.lonRange[1]
+                ) {
+                    setNation(loc.name);
+                    break;
+                }
+            }
+        };
+
+        if (location.latitude && location.longitude) {
+            checkNation();
+        }
+    }, [location]);
+
     return (
         <>
             <div className={style.content}>

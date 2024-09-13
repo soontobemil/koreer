1a81fc254c8202aa94b4e5406776365ffdabf496
chhong <pnci1029@gmail.com>
2024-09-13 10:58:24 +0900

    console test

--- koreer-fe/src/components/signup/SignUp.tsx
+++ koreer-fe/src/components/signup/SignUp.tsx
@@ -11,6 +11,7 @@ import {createUser} from "../../slice/signupSlice";
 import {UserPostDTO} from "../../types/signup";
 
 export function SignUp() {
+    console.log(11)
     const [nation, setNation] = useState('Select your country!');
     const [id, setId] = useState('');
     const [nickName, setNickName] = useState('');

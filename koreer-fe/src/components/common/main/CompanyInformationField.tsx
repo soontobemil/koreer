b71e195a946152c3d59805ee44563f6050293959
chhong <pnci1029@gmail.com>
2024-09-22 13:17:39 +0900

    KOR-30 republishing main page ui(header & first view of homepage)

--- koreer-fe/src/components/common/main/CompanyInformationField.tsx
+++ koreer-fe/src/components/common/main/CompanyInformationField.tsx
@@ -1,15 +1,22 @@
 import style from "../../../assets/scss/common/main.module.scss";
+import blackBackgroundLogo from "../../../assets/img/logo_with_black_background.png"
 
 export function CompanyInformationField() {
     return(
         <>
-            <div className={style.questionContainer}>
-                <div className={style.questionText}>북미 취업을 원해?</div>
-                <div className={`${style.arrowImage} ${style.arrowImageRight}`}></div>
-                <div className={`${style.arrowImage} ${style.arrowImageLeft}`}></div>
+            <div className={style.container}>
+                <div className={style.headingText}>Dreaming of an Exciting Career Abroad?</div>
+                <div className={style.subText}>Discover opportunities in the global market and elevate your career to
+                    new heights.
+                </div>
+                <div className={style.imageBanner}>
+                    <img src={blackBackgroundLogo} alt="Global Opportunities" className={style.bannerImage}/>
+                </div>
+                <div className={style.callToAction}>Where would you like to explore your future career?</div>
                 <div className={style.buttonsContainer}>
-                    <button className={style.questionButton}>Canada</button>
-                    <button className={style.questionButton}>USA</button>
+                    <button className={style.choiceButton}>USA</button>
+                    <button className={style.choiceButton}>CANADA</button>
+                    <button className={style.choiceButton}>Venture to North America</button>
                 </div>
             </div>
         </>

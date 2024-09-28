0923efead98cc196f8733fa4e3bcef7a448cbdbc
chhong <pnci1029@gmail.com>
2024-09-28 15:22:23 +0900

    add footer component about koreer

--- koreer-fe/src/components/common/Main.tsx
+++ koreer-fe/src/components/common/Main.tsx
@@ -15,6 +15,8 @@ export default function Main() {
                 {/* 북미 회사 조회 */}
                 <CompanyInformationField/>
 
+                <div className={style.underLine}/>
+
                 {/*  커뮤니티 컴포넌트 분리예정  */}
                 <div className={style.mainCommunityWrapper}>
                     <div className={style.communityTitleWrapper}>
@@ -25,14 +27,15 @@ export default function Main() {
                     <ListItems/>
                 </div>
 
+                <div className={style.underLine}/>
 
                 {/*  북미 취업 과정 컴포넌트  */}
                 <HiringProcessField/>
 
                 {/*  평가 및 후기  */}
-                <TestimonialField />
+                {/*<TestimonialField />*/}
             </div>
-            <TopButton />
+            <TopButton/>
         </>
     )
 }

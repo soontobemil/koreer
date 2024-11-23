09bb3e98264fb79f30ade2509c4a53772d141e83
yeahsdev <98158230+yeahsdev@users.noreply.github.com>
2024-11-23 15:17:57 +0900

    Create apiUrlMiddleware.js

---
+++ koreer-be/src/middlewares/apiUrlMiddleware.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+require('dotenv').config();
+
+// .env 파일에서 API_URL 가져오기
+const API_URL = process.env.API_URL;
+
+// 요청이 들어올 때마다 API_URL을 자동으로 붙이는 미들웨어
+const addApiUrlToRequest = (req, res, next) => {
+  req.fullUrl = `${API_URL}${req.originalUrl}`;
+  console.log(`Full URL with API_URL: ${req.fullUrl}`); // 요청된 URL과 결합된 API_URL 확인
+  next();
+};
+
+module.exports = addApiUrlToRequest;

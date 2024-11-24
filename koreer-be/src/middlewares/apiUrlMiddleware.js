a6d9b26da8c1c5308c4d4a2bd887ed1715fe0225
yeahsdev <iyeahs71@gmail>
2024-11-24 23:21:10 +0900

    modify apiUrlMiddleware

--- koreer-be/src/middlewares/apiUrlMiddleware.js
+++ koreer-be/src/middlewares/apiUrlMiddleware.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const API_URL = process.env.API_URL;
 
 // 요청이 들어올 때마다 API_URL을 자동으로 붙이는 미들웨어
-const addApiUrlToRequest = (req, res, next) => {
+const apiUrlToRequest = (req, res, next) => {
   req.fullUrl = `${API_URL}${req.originalUrl}`;
   console.log(`Full URL with API_URL: ${req.fullUrl}`); // 요청된 URL과 결합된 API_URL 확인
   next();

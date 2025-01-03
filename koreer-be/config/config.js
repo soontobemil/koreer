b9565bf6642507d095c969843c7d22a7d6bb9778
yeahsdev <iyeahs71@gmail>
2025-01-03 18:09:15 +0900

    Separate Railway for the development databse and the production database

--- koreer-be/config/config.js
+++ koreer-be/config/config.js
@@ -2,20 +2,20 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
+    // add other configurations
+    development : {
+      use_db_url: 'DATABASE_URL',
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: false // SSL 비활성화
+      }
+    },
+    production: {
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       host: process.env.DB_HOST,
       dialect: 'postgres'
     },
-    // add other configurations
-    railway : {
-      use_db_url: 'DATABASE_URL',
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: false // SSL 비활성화
-      }
-    }
   };
   
--- koreer-be/models/index.js
+++ koreer-be/models/index.js
@@ -4,8 +4,8 @@ const config = require('../config/config');
 const fs = require('fs');
 const path = require('path');
 
-const env = 'railway';
-//const env = process.env.NODE_ENV || 'development';;
+//const env = 'railway';
+const env = process.env.NODE_ENV || 'development';;
 const dbConfig = config[env];
 
 let sequelize;

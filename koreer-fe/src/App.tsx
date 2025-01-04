138f113c1b7a6073a75a5b02e09bbccb883be80e
soontobemil <yourthought2020@gmail.com>
2025-01-03 17:44:31 -0800

    Improve UIUX

--- koreer-fe/src/App.tsx
+++ koreer-fe/src/App.tsx
@@ -1,10 +1,18 @@
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./components/common/Header";
 import { Footer } from "./components/common/Footer";
 import theme from './theme';
+import { useEffect } from 'react';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Scroll to top on route change
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
--- koreer-fe/src/index.tsx
+++ koreer-fe/src/index.tsx
@@ -1,85 +1,78 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Main from "./components/common/Main";
-import App from "./App";
-import {Community} from "./features/community/Community";
-import {Contact} from "./components/contactus/Contact";
-import {AboutUs} from "./components/aboutus/AboutUs";
-import {CompanyInformation} from "./features/company/CompanyInformation";
+import App from './App';
 import {Provider} from 'react-redux';
-import {store} from "./slice";
-import {SignIn} from "./features/auth/SignIn";
-import {SignUp} from "./features/auth/SignUp";
-import {NotFoundPage} from "./components/common/NotFoundPage";
-import {Tips} from "./features/community/Tips";
-import {TipsPosting} from "./features/community/TipsPosting";
-import {Success} from "./components/common/Success";
-import {EmploymentInfo} from "./features/employment/EmploymentInfo";
-import {USAVisaInfo} from "./features/visa/USAVisaInfo";
-import {CanadaVisaInfo} from "./features/visa/CanadaVisaInfo";
-import {USASalaryInfo} from "./components/salary/USASalaryInfo";
-import {CanadaSalaryInfo} from "./components/salary/CanadaSalaryInfo";
-import {InterviewGuide} from "./features/employment/InterviewGuide";
-import {USALifeInfo} from "./components/life/USALifeInfo";
-import {CanadaLifeInfo} from "./components/life/CanadaLifeInfo";
-import {WorkPermit} from "./features/employment/WorkPermit";
-import {CommunityForm} from "./features/community/CommunityForm";
-import {Seminar} from "./components/seminar/Seminar";
+import {store} from './store';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Main from "./components/common/Main";
+import {Seminar} from './components/seminar/Seminar';
+import {CompanyInformation} from './features/company/CompanyInformation';
+import {EmploymentInfo} from './features/employment/EmploymentInfo';
+import {USAVisaInfo} from './features/visa/USAVisaInfo';
+import {Success} from './components/common/Success';
+import {NotFoundPage} from './components/common/NotFoundPage';
 import {TestComponents} from './components/shared/TestComponents';
 import {MembershipPage} from './features/membership/MembershipPage';
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Main />
+      },
+      {
+        path: "test",
+        element: <TestComponents />
+      },
+      {
+        path: "seminar-info",
+        element: <Seminar />
+      },
+      {
+        path: "company-information",
+        element: <CompanyInformation />
+      },
+      {
+        path: "employment-info",
+        element: <EmploymentInfo />
+      },
+      {
+        path: "visa-info/usa",
+        element: <USAVisaInfo />
+      },
+      {
+        path: "success",
+        element: <Success />
+      },
+      {
+        path: "membership",
+        element: <MembershipPage />
+      }
+    ]
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />
+  }
+]);
 
-root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<App/>}>
-                        <Route path="" element={<Main/>}/>
-                        <Route path="test" element={<TestComponents/>}/>
-                        <Route path="seminar-info" element={<Seminar/>}/>
-                        <Route path="company-information" element={<CompanyInformation/>}/>
-                        <Route path="employment-info" element={<EmploymentInfo/>}/>
-                        <Route path="visa-info">
-                            <Route path="usa" element={<USAVisaInfo/>}/>
-                            <Route path="canada" element={<CanadaVisaInfo/>}/>
-                        </Route>
-                        <Route path="salary-info">
-                            <Route path="usa" element={<USASalaryInfo/>}/>
-                            <Route path="canada" element={<CanadaSalaryInfo/>}/>
-                        </Route>
-                        <Route path="life-info">
-                            <Route path="usa" element={<USALifeInfo/>}/>
-                            <Route path="canada" element={<CanadaLifeInfo/>}/>
-                        </Route>
-                        <Route path="work-permit" element={<WorkPermit/>}/>
-                        <Route path="interview-guide">
-                            <Route path="" element={<InterviewGuide/>}/>
-                            <Route path="technical" element={<InterviewGuide/>}/>
-                            <Route path="behavioral" element={<InterviewGuide/>}/>
-                            <Route path="coding-test" element={<InterviewGuide/>}/>
-                        </Route>
-                        <Route path="community" element={<Community/>} />
-                        <Route path="community/post" element={<CommunityForm />} />
-                        <Route path="tips" element={<Tips/>}/>
-                        <Route path="tips/posting" element={<TipsPosting/>}/>
-                        <Route path="about-us" element={<AboutUs/>}/>
-                        <Route path="contact" element={<Contact/>}/>
-                        <Route path="signin" element={<SignIn/>}/>
-                        <Route path="signup" element={<SignUp/>}/>
-                        <Route path="success" element={<Success/>}/>
-                        <Route path="membership" element={<MembershipPage/>}/>
-                    </Route>
-                    <Route path="*" element={<NotFoundPage/>}/>
-                </Routes>
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+// Add scroll to top on route change
+router.subscribe(() => {
+  window.scrollTo(0, 0);
+});
+
+const root = createRoot(
+  document.getElementById('root') as HTMLElement
 );
 
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);

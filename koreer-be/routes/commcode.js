da46aa59720481f1edad2092817c45fe36faee26
yeahsdev <iyeahs71@gmail>
2025-02-19 16:41:05 +0900

    Modify Common Code API logic as Get All Common Code

--- koreer-be/routes/commcode.js
+++ koreer-be/routes/commcode.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const commCodeController = require('../controllers/commCodeController');
 
-router.get('/:groupCode', commCodeController.getCommonCodes);
+router.get('/:groupCode?', commCodeController.getCommonCodes);
 
 module.exports = router;
